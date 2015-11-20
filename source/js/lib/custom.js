6dde99b8cc383bc6b5b3b3f4b8df45532b2da2be
Sabino Velasquez <sabino@Sabinos-MacBook-Pro.local>
2015-11-20 20:15:12 -0300

    css icons, structure

--- source/js/lib/custom.js
+++ source/js/lib/custom.js
@@ -1,4 +1,11 @@
 'use strict';
 var app = angular
 
-	.module('app', ['ngAnimate','ui.bootstrap']);
+	.module('app', ['ngAnimate','ui.bootstrap'])
+	.controller( 'headerCtrl', ['$scope', function($scope) {
+		
+		$scope.init = function() {
+			$scope.initial = true;
+		}
+
+	}] );
