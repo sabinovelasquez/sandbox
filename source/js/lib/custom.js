ca29f55edfb35c034106179a1fa532453a032836
Sabino Velasquez <sabino@Sabinos-MacBook-Pro.local>
2015-11-21 12:11:11 -0300

    working form: mandrillapp + angularjs

--- source/js/lib/custom.js
+++ source/js/lib/custom.js
@@ -2,10 +2,50 @@
 var app = angular
 
 	.module('app', ['ngAnimate','ui.bootstrap'])
+
 	.controller( 'headerCtrl', ['$scope', function($scope) {
 		
 		$scope.init = function() {
 			$scope.initial = true;
+		}
+
+	}] )
+	.controller( 'contactCtrl', ['$scope', '$http', function($scope, $http) {
+		
+		var mandrill_api = 'VaXfmvT99HCIcEF8MfyflA';
+		
+		$scope.sended = false;
+		$scope.sended_err = false;
+		
+		$scope.submitContacto = function() {
+			
+			var m = new mandrill.Mandrill(mandrill_api);
+
+			var email = $scope.contacto.email.$viewValue;
+			var subject = $scope.contacto.subject.$viewValue;
+			var message = $scope.contacto.message.$viewValue;
+
+			var params = {
+				"message": {
+					"from_email":"no-reply@sandbox.cl",
+					"to":[
+						{"email": email }
+					],
+					"subject": subject,
+					"text": message
+				}
+			};
+			
+			m.messages.send(params, function(res) {
+				$scope.$apply(function () {
+					$scope.sended = true;
+				});
+			}, function(err) {
+				$scope.$apply(function () {
+					$scope.sended_err = true;
+				});
+			});
+
 		}
 
 	}] );
